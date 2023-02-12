06c1cb02af6369bfc6cf8afa9085aaf74d156050
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-12 13:48:08 +0100

    added loading dots and error handling

--- youtubeConverter/src/components/UrlForm.tsx
+++ youtubeConverter/src/components/UrlForm.tsx
@@ -1,21 +1,33 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import axios from 'axios'
 import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
-
+import BeatLoader from "react-spinners/BeatLoader";
 const UrlForm = () => {
   const [videoUrl,setVideoUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  let theme = JSON.parse(localStorage.getItem('components.theme')!)
 
+
+  useEffect( () =>{
+    theme = JSON.parse(localStorage.getItem('components.theme')!)
+  })
 
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true)
     handleDownload()
     setVideoUrl('')
 
   }
   const handleDownload = async () => {
-    const response = await axios.post('http://127.0.0.1:5000/download_video', { video_url: videoUrl });
-    console.log(response.data);
-
+    try{
+      const response = await axios.post('http://127.0.0.1:5000/download_video', { video_url: videoUrl });
+      console.log(response.data);
+    }catch(error){
+      console.error(error);
+    } finally {
+    setLoading(false);
+    }
   }
 
   return (
@@ -43,13 +55,31 @@ const UrlForm = () => {
             Enter URL
           </label>
         </div>
-        <button
-          className="btn"
-          aria-label="Enter URL"
-          type="submit"
-        >
-          <DocumentArrowDownIcon />
-        </button>
+        {
+          loading?
+          (
+
+            <button
+              className='btn'>
+              
+              <BeatLoader
+                color={theme === "dark"? "#16161d" : "#ecebf4"}
+                loading={loading}
+                size={5}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            </button>
+          ):(
+          <button
+            className="btn"
+            aria-label="Enter URL"
+            type="submit"
+          >
+            <DocumentArrowDownIcon />
+          </button>
+          )
+        }
       </form>
 
     </div>
