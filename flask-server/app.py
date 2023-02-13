5bda75af514d3453006828b31933376f0fe46965
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-13 22:24:25 +0100

    changed to send_file as response

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request, send_file,jsonify, make_response, Response
 from pytube import YouTube
 from flask_cors import CORS
 app = Flask(__name__)
@@ -7,15 +7,14 @@ CORS(app)
 @app.route('/download_video', methods=['POST'])
 def handleDownload():
   try:
-    # Get the video URL from the request body
+    #Get the video URL from the request body
     video_url = request.get_json()['video_url']
     # Use the pytube library to download the video
     video = YouTube(video_url)
-    download_path = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    download_path = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').first().download()
     filename = download_path.split("//")[-1]
-    # Return a success status to the front-end
-    return jsonify({'status': 'success','download_path': download_path,'file':filename})
-
+    return send_file(download_path, as_attachment=True,download_name=filename,mimetype="video/mp4")
+    
   except Exception as e:
     # Return a generic error status for all other exceptions
     return jsonify({'status': 'error', 'message': 'An unexpected error occurred', 'url': video_url}), 500
