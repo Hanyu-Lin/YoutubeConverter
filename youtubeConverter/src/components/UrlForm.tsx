19ae0f69bcd616a0b7812ca3c0fbca5fbc98c143
Hanyu-Lin <hanyulin02@gmail.com>
2023-02-21 08:45:04 +0100

    remove autocomplete on input

--- youtubeConverter/src/components/UrlForm.tsx
+++ youtubeConverter/src/components/UrlForm.tsx
@@ -1,56 +1,51 @@
-import React,{useState} from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../redux/store';
-import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
 import BeatLoader from "react-spinners/BeatLoader";
 const UrlForm = () => {
-  const [videoUrl,setVideoUrl] = useState('');
+  const [videoUrl, setVideoUrl] = useState("");
   const [loading, setLoading] = useState(false);
-  const { theme } = useSelector((state: RootState) => state.theme)
+  const { theme } = useSelector((state: RootState) => state.theme);
 
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
     handleDownload();
-    setVideoUrl('')
-
-  }
+    setVideoUrl("");
+  };
   const handleDownload = async () => {
-    try{
-      const response = await fetch('http://127.0.0.1:5000/download_video', {
-        method: 'POST',
+    try {
+      const response = await fetch("http://127.0.0.1:5000/download_video", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ video_url: videoUrl }),
       });
       const blob = await response.blob();
-      console.log(response.headers)
+      console.log(response.headers);
       const url = URL.createObjectURL(blob);
-      openDownloadedFile(url,"video")
-
-    }catch(error){
+      openDownloadedFile(url, "video");
+    } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
-  }
-  const openDownloadedFile = (url :string, fileName: string) => {
-    const link = document.createElement('a');
+  };
+  const openDownloadedFile = (url: string, fileName: string) => {
+    const link = document.createElement("a");
     link.href = url;
     link.download = `${fileName}.mp4`;
     document.body.appendChild(link);
     link.click();
     link.remove();
-  }
+  };
 
   return (
     <div>
-      <form
-        className="form"
-        onSubmit={handleFormSubmit}
-      >
-        <div className="wrapper"> 
+      <form className="form" onSubmit={handleFormSubmit}>
+        <div className="wrapper">
           <input
             type="text"
             className="input"
@@ -59,45 +54,32 @@ const UrlForm = () => {
             onInput={(e) => setVideoUrl(e.currentTarget.value)}
             required
             autoFocus
+            autoComplete="false"
             maxLength={100}
             placeholder="Enter URL"
           />
-          <label
-            htmlFor="inputField"
-            className="label"
-          >
+          <label htmlFor="inputField" className="label">
             Enter URL
           </label>
         </div>
-        {
-          loading?
-          (
-
-            <button
-              className='btn'>
-              
-              <BeatLoader
-                color={theme === "dark"? "#16161d" : "#ecebf4"}
-                loading={loading}
-                size={5}
-                aria-label="Loading Spinner"
-                data-testid="loader"
-              />
-            </button>
-          ):(
-          <button
-            className="btn"
-            aria-label="Enter URL"
-            type="submit"
-          >
+        {loading ? (
+          <button className="btn">
+            <BeatLoader
+              color={theme === "dark" ? "#16161d" : "#ecebf4"}
+              loading={loading}
+              size={5}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </button>
+        ) : (
+          <button className="btn" aria-label="Enter URL" type="submit">
             <DocumentArrowDownIcon />
           </button>
-          )
-        }
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default UrlForm
-
+export default UrlForm;
